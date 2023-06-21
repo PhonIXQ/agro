430e679943bbe261a2fa9c32c8557f7f3ded3236
PhonIXQ <logophonixo@hotmail.com>
2023-06-21 23:11:09 +0700

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
    return (
       <div className="App">
          <header className="App-header">
+            <h2>argo PhonIXQ</h2>
             <img src={logo} className="App-logo" alt="logo" />
             <p>
                Edit <code>src/App.js</code> and save to reload.
