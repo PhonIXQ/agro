1dcf4cea243c005fbf36a6c3ed4cb568e6891a7c
PhonIXQ <logophonixo@hotmail.com>
2023-06-30 14:39:01 +0700

    change diffDay to function

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,16 @@ import './App.css';
 function App() {
    let today = new Date().toISOString().slice(0, 10);
 
-   const startDate = '2023-05-18';
-   const startDate0 = '2023-05-08';
-   const startDate1 = '2023-06-21';
-   const endDate = today;
+   const diDay = (date) => {
+      const diMs = new Date(today) - new Date(date);
+      const diDays = diMs / (1000 * 60 * 60 * 24);
+      return diDays;
+   };
 
-   const diffInMs = new Date(endDate) - new Date(startDate);
-   const diffInMs0 = new Date(endDate) - new Date(startDate0);
-   const diffInMs1 = new Date(endDate) - new Date(startDate1);
-   const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
-   const diffInDays0 = diffInMs0 / (1000 * 60 * 60 * 24);
-   const diffInDays1 = diffInMs1 / (1000 * 60 * 60 * 24);
+   const date20230518 = '2023-05-18';
+   const date20230508 = '2023-05-08';
+   const date20230621 = '2023-06-21';
+   // const date20230630 = '2023-06-30';
 
    return (
       <div className="App">
@@ -23,46 +22,47 @@ function App() {
             <img src={logo} className="App-logo" alt="logo" />
             <h3>.</h3>
          </header>
-
-         <table>
-            <thead>
-               <tr>
-                  <th>Date</th>
-                  <th>#</th>
-                  <th>Name</th>
-                  <th></th>
-               </tr>
-            </thead>
-            <tbody>
-               <tr>
-                  <td>2023-05-08</td>
-                  <td>Beta</td>
-                  <td>Rocket</td>
-                  <td className="tdlast">{diffInDays0} Day</td>
-               </tr>
-               <tr>
-                  <td>2023-05-18</td>
-                  <td>Pand</td>
-                  <td>
-                     <ol>
-                        <li>Butterhead</li>
-                     </ol>
-                  </td>
-                  <td className="tdlast">{diffInDays} Day</td>
-               </tr>
-               <tr>
-                  <td>2023-06-21</td>
-                  <td>First</td>
-                  <td>
-                     <ol>
-                        <li>Green Cos</li>
-                        {/* <li>Red Cos</li> */}
-                     </ol>
-                  </td>
-                  <td className="tdlast">{diffInDays1} Day</td>
-               </tr>
-            </tbody>
-         </table>
+         <section>
+            <table>
+               <thead>
+                  <tr>
+                     <th>Date</th>
+                     <th>#</th>
+                     <th>Name</th>
+                     <th></th>
+                  </tr>
+               </thead>
+               <tbody>
+                  <tr>
+                     <td>2023-05-08</td>
+                     <td>Beta</td>
+                     <td>Rocket</td>
+                     <td className="tdlast">{diDay(date20230508)} Day</td>
+                  </tr>
+                  <tr>
+                     <td>2023-05-18</td>
+                     <td>Pand</td>
+                     <td>
+                        <ol>
+                           <li>Butterhead</li>
+                        </ol>
+                     </td>
+                     <td className="tdlast">{diDay(date20230518)} Day</td>
+                  </tr>
+                  <tr>
+                     <td>2023-06-21</td>
+                     <td>First</td>
+                     <td>
+                        <ol>
+                           <li>Green Cos</li>
+                           {/* <li>Red Cos</li> */}
+                        </ol>
+                     </td>
+                     <td className="tdlast">{diDay(date20230621)} Day</td>
+                  </tr>
+               </tbody>
+            </table>
+         </section>
 
          <footer>
             <a href="/">PhonIXQ</a> <span>&copy; 2023</span>
